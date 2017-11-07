140f74f15f9ff49e011225ecedd285a7ab8e9bf7
Canglangweiwei <weiweistar@163.com>
2017-11-07 11:26:41 +0800

    优化部分功能

--- library/common/src/main/java/com/jaydenxiao/common/commonutils/NetUtil.java
+++ library/common/src/main/java/com/jaydenxiao/common/commonutils/NetUtil.java
@@ -4,12 +4,8 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.annotation.IntDef;
-import android.text.TextUtils;
 
 import com.jaydenxiao.common.base.BaseApplication;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * des:网络管理工具
@@ -40,19 +36,5 @@ public class NetUtil {
             }
         }
         return NETWORK_TYPE_NONE;
-    }
-
-    /**
-     * 判断网址是否有效
-     */
-    public static String isLinkAvailable(String link) {
-        if (TextUtils.isEmpty(link))
-            return "";
-        Pattern pattern = Pattern.compile("^(http://|https://)?((?:[A-Za-z0-9]+-[A-Za-z0-9]+|[A-Za-z0-9]+)\\.)+([A-Za-z]+)[/\\?\\:]?.*$", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(link);
-        if (matcher.matches()) {
-            return link;
-        }
-        return "";
     }
 }
